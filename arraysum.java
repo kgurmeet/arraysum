4ab4b240c41b84c03a24e76b4de5efa5ec7a1793
kgurmeet <131999481+kgurmeet@users.noreply.github.com>
2023-07-16 21:09:51 +0530

    Add files via upload

---
+++ arraysum.java
@@ -0,0 +1,12 @@
+public class arraysum {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int sum=0;
+        
+        for( int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println("The sum is "+sum);
+        
+    }
+}
